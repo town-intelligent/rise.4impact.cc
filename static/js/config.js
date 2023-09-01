e081277ede86c4d9fc0d7a4403480615422885a2
yillkid <yillkid@gmail.com>
2023-09-01 20:30:01 +0800

    member add

--- static/js/config.js
+++ static/js/config.js
@@ -6,7 +6,7 @@ HOST_URL_TPLANET_DAEMON = "https://beta-tplanet-backend.townway.com.tw";
 HOST_URL_EID = "https://alpha-eid.townway.com.tw";
 
 // Site hoster
-SITE_HOSTERS = ["ti@4impact.cc", "yunuscentre@yunustw.org"]
+SITE_HOSTERS = ["ti@4impact.cc", "yunuscentre@yunustw.org", "tengmj@ncut.edu.tw"]
 
 // Site information
 SITE_TYPE = 0 // 0 or 1(公板)
